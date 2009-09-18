71d94994fa080c13c64c5059dacf4359fa405969
Ryan McGeary <ryanongit@mcgeary.org>
2009-09-18 14:10:37 +0800

    Added support for passing an :extra param to flickr.auth.url
    * E.g. flickr.auth.url(:write, :extra => "http://subdomain.example.com/final_flickr_callback")
    * This is an undocumented feature of the Flickr API, but allows for a somewhat
    elegant solution to the inability to specify a dynamical callback url.  Put your
    callback URL in the :extra parameter and redirect to it after your main callback
    is hit.

--- lib/flickr/auth.rb
+++ lib/flickr/auth.rb
@@ -7,43 +7,47 @@ class Flickr::Auth < Flickr::Base
   def frob
     @frob ||= get_frob
   end
-  
+
   # set the frob
   def frob= frob
     @frob=frob
   end
 
   # generates the authorization url to allow access to a flickr account.
-  # 
+  #
   # Params
   # * perms (Optional)
   #     sets the permision level to grant on the flickr account.
   #       :read - permission to read private information (DEFAULT)
   #       :write - permission to add, edit and delete photo metadata (includes 'read')
   #       :delete - permission to delete photos (includes 'write' and 'read')
-  # 
-  def url(perms = :read)
-    options = {:api_key => @flickr.api_key, :perms => perms, :frob => self.frob}
+  # * options (Optional)
+  #     additional parameters to pass to flickr.  The most common use-case for
+  #     this is to pass the (undocumented) :extra parameter that is returned
+  #     back from flickr during the callback.
+  #
+  def url(perms = :read, options = {})
+    options.merge!(:api_key => @flickr.api_key, :perms => perms, :frob => self.frob)
     @flickr.sign_request(options)
-    Flickr::Base::AUTH_ENDPOINT + "?" + options.collect{|k,v| "#{k}=#{v}"}.join('&')
+    Flickr::Base::AUTH_ENDPOINT + "?" + options.collect{|k,v| "#{k}=#{CGI.escape(v.to_s)}"}.join('&')
   end
 
   # gets the token object for the current frob
-  # 
+  #
   # Params
   # * pass_through (Optional)
   #     Boolean value that determines if a call will be made to flickr to find a taken for the current frob if empty
-  # 
+  #
   def token(pass_through = true)
     @token ||= get_token(pass_through) rescue nil
   end
 
   # saves the current token to the cache file if token exists
-  # 
+  #
   # Param
   # * filename (Optional)
   #     filename of the cache file. defaults to the file passed into Flickr.new
-  # 
+  #
   def cache_token(filename = @flickr.token_cache)
     if filename and self.token
       cache_file = File.open(filename, 'w+')
